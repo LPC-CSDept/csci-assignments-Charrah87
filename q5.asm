2acf7807d906dbe00d2ad087cc9d069e6083f9f3
Charrah87 <94938480+Charrah87@users.noreply.github.com>
2021-12-14 16:42:33 -0800

    added test LSB segment

--- q5.asm
+++ q5.asm
@@ -14,8 +14,11 @@ num     .word 3
         .globl main
 
 main:
-        lw  $t9, num        # Load num into $t9 which ='s 3
-        lui $t0, 0xffff     # Load upper immediate
+        lw      $t9, num                # Load num into $t9 which ='s 3
+        lui     $t0, 0xffff             # Load upper immediate
 
 rdy_wait:
-        lw  
+        lw	$t1, 0($t0)
+	andi    $t1, $t1, 0x0001 	# test RC register has 1 in LSB
+	beq	$t1, $zero, rd_wait     # if LSB is 0, branch rd_wait
+	lw	$s0, 4($t0)		# if LSB is 1, read a character from I/O device
