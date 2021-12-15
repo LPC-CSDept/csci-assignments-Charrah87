0427b4702b56a7cc8375958290bc51a3fe824929
Charrah87 <94938480+Charrah87@users.noreply.github.com>
2021-12-14 16:37:46 -0800

    load inital value

--- q5.asm
+++ q5.asm
@@ -8,10 +8,14 @@
 #
 
         .data
-num     .word 2
+num     .word 3
 
         .text
         .globl main
 
 main:
-        
+        lw  $t9, num        # Load num into $t9 which ='s 3
+        lui $t0, 0xffff     # Load upper immediate
+
+rdy_wait:
+        lw  
