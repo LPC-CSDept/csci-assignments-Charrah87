87c8006dee6aca96ed51c81452f6b410b92912ae
Charrah87 <94938480+Charrah87@users.noreply.github.com>
2021-12-15 20:57:11 -0800

    almost done

--- final1.asm
+++ final1.asm
@@ -7,30 +7,42 @@
 # Take 3 digits through MM I/O. Make the decimal value for the 3 digit input
 #
 
-        .data
-num:     .word 3
+	.data
+num:	.word 3
+x:	.word 100
 
         .text
-        .globl main
+        .globl  main
 
 main:
-        lw      $t9, num                # Load num into $t9 which ='s 3
-        lui     $t0, 0xffff             # Load upper immediate
+	lw 	$t9, num  		# three character
+	lw	$t8, x
+	ori	$s1, $zero, 0		# Load 12 into $t2
+
+	lui 	$t0, 0xffff 		# lui : load upper immediate
 
 rdy_wait:
-        lw	$t1, 0($t0)
-	andi    $t1, $t1, 0x0001 	# test RC register has 1 in LSB
-	beq	$t1, $zero, rdy_wait    # if LSB is 0, branch rd_wait
-	lw	$s0, 4($t0)             # if LSB is 1, read a character from I/O device
+	lw	$t1, 0($t0)
+	andi 	$t1, $t1, 0x0001 	# test the Receiver control register 				     # has 1 in LSB # ready?
+	beq	$t1, $zero, rdy_wait 	# if LSB is 0, branch rdy_wait
+	lw	$s0, 4($t0)		# if LSB is 1, read a character from IO
 
-	sub 	$s0, $s0, 48 		# subtract ASCII code for '0'
-	sub 	$t9, $t9, 1             # Subtract 1 from count of 3 characters
-	beq 	$t9, $zero, readall     
-	mul 	$s1, $s0, 10 		# $s1 is the last digit entered
-	nop
-	add	$s1, $s0, $s1		# Add the first and second characters
-	b 	rdy_wait
+	sub 	$s0, $s0, 48 		# subtract ASCII code for '0' ; '3' - 			      	      # '0' = 3
 
-        sub 	$t9, $t9, 1
+	sub 	$t9, $t9, 1
 	beq 	$t9, $zero, readall
-	b 	rd_wait
+	mul	$s0, $s0, $t8
+	nop
+	add	$s1, $s0, $s1
+	div	$t8, $t8, 10
+	nop
+
+	b 	rdy_wait		# branch back to rdy_wait
+
+
+readall:
+	add 	$a0, $s1, $s0
+	li 	$v0, 1
+	syscall 
+	li 	$v0, 10
+	syscall
