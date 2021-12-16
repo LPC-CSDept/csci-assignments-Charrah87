861f68397c063e91ad5c4bd5e56074316641eb7b
Charrah87 <94938480+Charrah87@users.noreply.github.com>
2021-12-15 18:20:35 -0800

    syntax fix

--- final1.asm
+++ final1.asm
@@ -8,7 +8,7 @@
 #
 
         .data
-num     .word 3
+num:     .word 3
 
         .text
         .globl main
@@ -20,5 +20,17 @@ main:
 rdy_wait:
         lw	$t1, 0($t0)
 	andi    $t1, $t1, 0x0001 	# test RC register has 1 in LSB
-	beq	$t1, $zero, rd_wait     # if LSB is 0, branch rd_wait
+	beq	$t1, $zero, rdy_wait    # if LSB is 0, branch rd_wait
 	lw	$s0, 4($t0)             # if LSB is 1, read a character from I/O device
+
+	sub 	$s0, $s0, 48 		# subtract ASCII code for '0'
+	sub 	$t9, $t9, 1             # Subtract 1 from count of 3 characters
+	beq 	$t9, $zero, readall     
+	mul 	$s1, $s0, 10 		# $s1 is the last digit entered
+	nop
+	add	$s1, $s0, $s1		# Add the first and second characters
+	b 	rdy_wait
+
+        sub 	$t9, $t9, 1
+	beq 	$t9, $zero, readall
+	b 	rd_wait
