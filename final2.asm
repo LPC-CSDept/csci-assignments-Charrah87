7b14f03c883e2dd606c64b3f20b6b6d956f2465e
Charrah87 <94938480+Charrah87@users.noreply.github.com>
2021-12-16 16:32:29 -0800

    Added "aneabled interrupt

---
+++ final2.asm
@@ -0,0 +1,21 @@
+#
+# Cory Harrah
+# 12/16/21
+#
+# Make the interrupt handler(kernal text program) that can quit when q is pressed
+#
+		    .kdata     		#   kernel data     
+s1:     	.word 10     
+s2:     	.word 11    
+new_line: 	.asciiz "\n"     
+		
+
+		    .text     
+		    .globl     main     
+main:     
+		    mfc0 	$a0, $12	    # read from the status register     
+		    ori     $a0,   0xff11   # enable all interrupts     
+		    mtc0 	$a0, $12        # write back to status register     
+		    lui     $t0, 0xFFFF     # $t0 =   0xFFFF0000     
+		    ori     $a0, $0, 2      # enable keyboard interrupt     
+		    sw     	$a0, 0($t0)     # write back to 0xFFFF0000
