595269287fbbc1f868730481a2bc563176bbc0cd
Charrah87 <94938480+Charrah87@users.noreply.github.com>
2021-12-16 16:05:16 -0800

    spacing issue fixed

--- final1.asm
+++ final1.asm
@@ -15,35 +15,35 @@ x:	.word 100
         .globl  main
 
 main:
-	lw 	$t9, num  		# three characters
-	lw	$t8, x                  # Initial value of 100
-	ori	$s1, $zero, 0		# Load 10 into $s1
+	lw 		$t9, num  				# three characters
+	lw		$t8, x                  # Initial value of 100
+	ori		$s1, $zero, 0			# Load 10 into $s1
 
-	lui 	$t0, 0xffff 		# lui : load upper immediate
+	lui 	$t0, 0xffff 			# lui : load upper immediate
 
 rdy_wait:
-	lw	$t1, 0($t0)
-	andi 	$t1, $t1, 0x0001 	# test the Receiver control register 				     
-                                        # has 1 in LSB # ready?
-	beq	$t1, $zero, rdy_wait 	# if LSB is 0, branch rdy_wait
-	lw	$s0, 4($t0)		# if LSB is 1, read a character from IO
+	lw		$t1, 0($t0)
+	andi 	$t1, $t1, 0x0001 		# test the Receiver control register 				     
+                                	# has 1 in LSB # ready?
+	beq		$t1, $zero, rdy_wait 	# if LSB is 0, branch rdy_wait
+	lw		$s0, 4($t0)				# if LSB is 1, read a character from IO
 
-	sub 	$s0, $s0, 48 		# subtract ASCII code for '0'
+	sub 	$s0, $s0, 48 			# subtract ASCII code for '0'
 
-	sub 	$t9, $t9, 1		# subtract 1 from counter
-	beq 	$t9, $zero, readall	# Branch to final add+print when =0
-	mul	$s0, $s0, $t8		# Multiply by $t8
+	sub 	$t9, $t9, 1				# subtract 1 from counter
+	beq 	$t9, $zero, readall		# Branch to final add+print when =0
+	mul		$s0, $s0, $t8			# Multiply by $t8
 	nop
-	add	$s1, $s0, $s1		# Add $s1 and $s0 together
-	div	$t8, $t8, 10		# Divide > 100 > 10 > 1
+	add		$s1, $s0, $s1			# Add $s1 and $s0 together
+	div		$t8, $t8, 10			# Divide > 100 > 10 > 1
 	nop
 
-	b 	rdy_wait		# branch back to rdy_wait
+	b 		rdy_wait				# branch back to rdy_wait
 
 
 readall:
-	add 	$a0, $s1, $s0		# Add the final digit and previous sum
-	li 	$v0, 1			# Print values
+	add 	$a0, $s1, $s0			# Add the final digit and previous sum
+	li 		$v0, 1					# Print values
 	syscall 
-	li 	$v0, 10			# Exit program
+	li 		$v0, 10					# Exit program
 	syscall
